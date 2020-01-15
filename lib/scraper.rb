91fbf065167a385fd3322c8964cb3a6f4c2db9a3
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:29:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,8 +12,8 @@ class Scraper
       #course.title = post.css("h2").text
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
-    end
-  end
+    #end
+  #end
   
   def print_courses
     self.make_courses
