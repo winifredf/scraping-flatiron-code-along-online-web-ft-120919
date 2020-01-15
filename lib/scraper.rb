3b1c8d891f5c057782cc4d04d6ea691f0e1f5039
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:23:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,9 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
   
   def print_courses
     self.make_courses
