a6b424ca690e9626f5c50a83a221365a26b9fae9
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:34:52 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -21,9 +21,11 @@ class Course
   end
   
   def make_courses
-  self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
-  
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+end  
