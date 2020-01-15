68b818b1e0cca72ff3f54976d685c7a01b6a71ad
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:29:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,8 @@ require_relative './course.rb'
 class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
+  end
+  
     #doc.css(".post").each do |post|
       #course = Course.new
       #course.title = post.css("h2").text
