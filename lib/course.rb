27883bf650a3f7817c02972ab10a82cf5d572735
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:35:05 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,31 @@
-
-
+class Course
+ 
+  attr_accessor :title, :schedule, :description
+ 
+  @@all = []
+ 
+  def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def get_courses
+  self.get_page.css(".post")
+  end
+  
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+end  
